ed8492c0501465203c0300524a2c386e2b8e55df
MRDark1rg <rativardiashvili@gmail.com>
2023-10-16 17:44:27 +0400

    For of return

---
+++ app.js
@@ -0,0 +1,58 @@
+// function sum(a, b){
+//     if (a > b){
+//         a - b;
+//     }
+    
+//     return a + b;
+
+// }
+
+
+// const multi = function(a, b) {
+
+
+//     return a * b;
+
+// }
+// console.log(sum(10,5));
+// console.log(sum(1,15));
+
+// console.log(multi(10,5));
+
+
+
+// const arrow = (a, b) => {
+//     return a ** b;
+// }
+// const arro = (a, b) => a ** b;
+
+
+// console.log(arrow(2, 3))
+// console.log(arro(2, 3))
+
+// const arr = [1,2,3,5];
+// const clone = [...arr];
+// clone.push(arr);
+// clone.push(11);
+// arr.push('test');
+
+
+
+// console.log(...arr)
+// console.log(...clone)
+// console.log(arr === clone)
+
+function sum(a, b, ...others) {
+    console.log (others);
+    let count = 0; 
+    for(let item of others) {
+        count+=item;
+    }
+
+    return (a * b) / count;
+}
+
+console.log(sum(1,2,3,4,5));
+
+
+
